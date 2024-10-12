a529f76f05d35fb9511acf9065463965a0f6aa9a
unknown <ashkan.eskandari.dev@gmail.com>
2024-10-12 20:03:56 +0330

    update for pip

--- libs/indoxGen_tensor/__init__.py
+++ libs/indoxGen_tensor/__init__.py
@@ -1,7 +1,17 @@
+# indoxGen_tensor/__init__.py
+
 from .GAN.gan import TabularGANTrainer
 from .GAN.config import TabularGANConfig
-from .GAN.evaluation import train_and_evaluate_classifier, evaluate_utility, evaluate_statistical_similarity, \
-    evaluate_privacy,plot_distributions,evaluate_data_drift
+from .GAN.evaluation import (
+    train_and_evaluate_classifier,
+    evaluate_utility,
+    evaluate_statistical_similarity,
+    evaluate_privacy,
+    plot_distributions,
+    evaluate_data_drift
+)
+
 import importlib.metadata
 
-__version__ = importlib.metadata.version("indoxGen_tenor")
+# Ensure the package name is correct here
+__version__ = importlib.metadata.version("indoxGen_tensor")
