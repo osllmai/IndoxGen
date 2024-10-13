34fe28856d652a567a4b3e637d31ab7ff7d64d61
unknown <ashkan.eskandari.dev@gmail.com>
2024-10-13 13:16:28 +0330

    versioning

--- libs/indoxGen_tensor/setup.py
+++ libs/indoxGen_tensor/setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='indoxGen-tensor',
-    version='0.0.2',
+    version='0.0.3',
     license='AGPL-3.0-or-later',
     packages=find_packages(where='libs', include=['indoxGen_tensor', 'indoxGen_tensor.*']),
     package_dir={'': 'libs'},
